4aabae590633a930261028677445043ca1190e2f
Solvita <solvitamur@gmail.com>
2023-01-03 23:13:37 +0200

    add week7 code

---
+++ Temperature/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Would you like to convert your temperature to Farenheit or Celcius? (F/C):");
+char userOperator = Char.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter temperature:");
+int temp = Int32.Parse(Console.ReadLine());
+
+
+switch (userOperator)
+{
+    case 'F':
+        ConvertToFarenheit(temp, 9/5);
+        break;
+    case 'C':
+        ConvertToCelsius(temp, 32);
+        break;
+    default:
+        Console.WriteLine("Invalid operator");
+        break;
+}
+
+static void ConvertToCelsius(int a, int b)
+{
+    Console.WriteLine($"{(a - b) / 1.8}");
+}
+
+static void ConvertToFarenheit(int a, int b)
+{
+    Console.WriteLine($"{a * b + 32}");
+}
---
+++ Week7Functions/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userchoice = Console.ReadLine();
+
+if (userchoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodbye();
+}
+
+
+
+PrintHello();
+PrintGoodbye();
+
+
+static void PrintHello() //funtion /method
+{
+    Console.WriteLine("Hello,world!");
+}
+
+static void PrintGoodbye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
